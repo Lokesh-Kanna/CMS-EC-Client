977da0c1e1ac0cc1e47a20e0a8b646f989ff481b
Lokesh-Kanna <lokeshkanna.work@gmail.com>
2022-01-13 17:42:26 +0530

    changed the card style

--- src/Components/Dashboars/Dashboard.js
+++ src/Components/Dashboars/Dashboard.js
@@ -15,6 +15,7 @@ function Dashboard({ datum }) {
               rating={data.yourRating}
               summary={data.summary}
               recommend={data.recommend}
+              img={data.img}
             />
           );
         })}
@@ -24,17 +25,35 @@ function Dashboard({ datum }) {
   );
 }
 
-function CardDisp({ title, type, auth, rating, summary, recommend }) {
+function CardDisp({ title, type, auth, rating, summary, recommend, img }) {
   return (
-    <div class="card">
-      <div class="card-header">{title}</div>
-      <div class="card-body">
-        <h5 class="card-title">
-          {type} by {auth}
-        </h5>
-        <p class="card-text">{summary}</p>
-        <p class="card-text">Would I recommend this? - {recommend}</p>
-        <p>My rating for this is {rating}/10</p>
+    <div class="card mb-3" style={{ maxWidth: 1000 }}>
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img
+            src={img}
+            class="img-fluid rounded-start"
+            style={{ maxHeight: 300, minWidth: 200, maxWidth: 200 }}
+          />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">{title}</h5>
+            <p class="card-text">
+              A {type} by {auth}
+            </p>
+            <p class="card-text">
+              My rating for this {type} is {rating}/10
+            </p>
+            <p class="card-text">{summary}</p>
+            <p class="card-text">
+              Will I recommend this {type}? - {recommend}
+            </p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -7,6 +7,7 @@ const data = [
     summary:
       "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.",
     recommend: "Yes",
+    img: "https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg",
   },
   {
     title: "Midnight Mass",
@@ -16,6 +17,7 @@ const data = [
     summary:
       "An isolated island community experiences miraculous events - and frightening omens - after the arrival of a charismatic, mysterious young priest.",
     recommend: "Yes",
+    img: "https://upload.wikimedia.org/wikipedia/en/d/da/Midnight_Mass_Poster.jpg",
   },
   {
     title: "Man's Search for Meaning",
@@ -25,6 +27,7 @@ const data = [
     summary:
       "Psychiatrist Viktor Frankl's memoir has riveted generations of readers with its descriptions of life in Nazi death camps and its lessons for spiritual survival. Based on his own experience and the stories of his patients, Frankl argues that we cannot avoid suffering but we can choose how to cope with it, find meaning in it, and move forward with renewed purpose. At the heart of his theory, known as logotherapy, is a conviction that the primary human drive is not pleasure but the pursuit of what we find meaningful. Man's Search for Meaning has become one of the most influential books in America; it continues to inspire us all to find significance in the very act of living.",
     recommend: "Yes",
+    img: "https://images-na.ssl-images-amazon.com/images/I/814rCOycUIL.jpg",
   },
 ];
 
