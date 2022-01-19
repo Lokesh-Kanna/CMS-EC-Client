5fb92117362c546b82ec342fc3f81c8bd14a9a46
Lokesh-Kanna <lokeshkanna.work@gmail.com>
2022-01-19 11:58:40 +0530

    Add post functionalities complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
 import * as React from "react";
+import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import { MiniDrawer } from "./MiniDrawer";
 import { Dashboard } from "./Components/Dashboars/Dashboard";
 import { NewPost } from "./Components/Post/Post";
@@ -10,14 +11,22 @@ import { EditPost } from "./Components/Edit/Edit";
 import data from "./data";
 
 function App() {
+  const [allData, setAllData] = useState(data);
+
+  const addData = (data) => {
+    setAllData(data);
+  };
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<MiniDrawer />} />
-        <Route path="/dashboard" element={<Dashboard datum={data} />} />
-        <Route path="/add-new-post" element={<NewPost />} />
-        <Route path="/media" element={<Media datum={data} />} />
-        <Route path="/profile" element={<Profile datum={data} />} />
+        <Route path="/dashboard" element={<Dashboard datum={allData} />} />
+        <Route
+          path="/add-new-post"
+          element={<NewPost datum={allData} addData={addData} />}
+        />
+        <Route path="/media" element={<Media datum={allData} />} />
+        <Route path="/profile" element={<Profile datum={allData} />} />
         <Route path="/edit-post/:id" element={<EditPost />} />
       </Routes>
     </div>
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,11 +1,21 @@
 import * as React from "react";
 import "./Post.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { MiniDrawer } from "../../MiniDrawer";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
-function NewPost() {
+function NewPost({ datum, addData }) {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState();
+  const [type, setType] = useState();
+  const [auth, setAuth] = useState();
+  const [rating, setRating] = useState();
+  const [recommend, setRecommend] = useState();
+  const [img, setImg] = useState();
+  const [summary, setSummary] = useState();
   return (
     <div>
       <MiniDrawer />
@@ -23,24 +33,28 @@ function NewPost() {
             id="outlined-basic"
             label="Title"
             variant="filled"
+            onChange={(event) => setTitle(event.target.value)}
           />
           <TextField
             className="inputfields"
             id="outlined-basic"
             label="Type"
             variant="filled"
+            onChange={(event) => setType(event.target.value)}
           />
           <TextField
             className="inputfields"
             id="outlined-basic"
             label="Author/Director"
             variant="filled"
+            onChange={(event) => setAuth(event.target.value)}
           />
           <TextField
             className="inputfields"
             id="outlined-basic"
             label="Your rating out of 10"
             variant="filled"
+            onChange={(event) => setRating(event.target.value)}
           />
 
           <TextField
@@ -48,12 +62,14 @@ function NewPost() {
             id="outlined-basic"
             label="Would you suggest this to others?"
             variant="filled"
+            onChange={(event) => setRecommend(event.target.value)}
           />
           <TextField
             className="inputfields"
             id="outlined-basic"
             label="Image URL"
             variant="filled"
+            onChange={(event) => setImg(event.target.value)}
           />
           <TextField
             id="standard-multiline-static"
@@ -62,8 +78,27 @@ function NewPost() {
             rows={4}
             defaultValue="Summary"
             variant="filled"
+            onChange={(event) => setSummary(event.target.value)}
           />
-          <Button variant="contained">Publish</Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              const newPost = {
+                title,
+                type,
+                auth,
+                rating,
+                recommend,
+                img,
+                summary,
+              };
+              const addedPost = [...datum, newPost];
+              addData(addedPost);
+              navigate("/dashboard");
+            }}
+          >
+            Publish
+          </Button>
         </Box>
       </div>
     </div>
